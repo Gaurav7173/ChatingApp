30e039533643e9a236c4f4446238a44eba99898c
Gaurav7173 <gaurav.kumar7173.com>
2025-11-25 06:59:20 +0530

    Routing

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,45 +1,15 @@
-import { HttpClient } from '@angular/common/http';
-import { Component, inject, OnInit, Signal, signal } from '@angular/core';
-import { lastValueFrom } from 'rxjs';
+import { Component, inject } from '@angular/core';
 import { Nav } from '../layout/nav/nav';
-import { AccountService } from '../core/services/account-service';
-import { Home } from '../feature/home/home';
-import { User } from '../types/user';
+import { Router, RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  imports: [Nav,Home],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
-export class App implements OnInit {
-  private accountService=inject(AccountService);
-  private http=inject(HttpClient);
-  protected title='Chating application';
-  protected members=signal<User[]>([]);
+export class App{
 
-  setCurrentUser(){
-    const userString=localStorage.getItem('user');
-    if(!userString) return;
-    const user=JSON.parse(userString);
-    this.accountService.currentUser.set(user);
-  }
-
-  async ngOnInit(){
-   this.members.set(await this.getMembers());
-    this.setCurrentUser();
-  }
-
-  async getMembers(){
-    try{
-      return lastValueFrom(this.http.get<User[]>('https://localhost:5001/api/members'));
-
-    }catch(error){
-      console.log(error);
-      throw error;
-    }
-  }
-    
+  protected router=inject(Router);
   
-
 }
